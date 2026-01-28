e3778bf38ed5906256cfb519af35bb96d7411846
Ondřej Synáček <ondrej@synacek.org>
2026-01-28 22:16:17 +0100

    bump heic-convert to 0.1.5

--- Formula/comatory-heic-convert.rb
+++ Formula/comatory-heic-convert.rb
@@ -1,14 +1,14 @@
 class ComatoryHeicConvert < Formula
   desc "Personalized HEIC to JPEG converter"
   homepage "https://github.com/comatory/heic-convert"
-  url "https://github.com/comatory/heic-convert/archive/refs/tags/0.1.3.tar.gz"
-  sha256 "0b7b62525322c23127037eb5a22b24be7204aae75be44aca99cc8d6477c2c6e4"
+  url "https://github.com/comatory/heic-convert/archive/refs/tags/0.1.5.tar.gz"
+  sha256 "8b834b08c9fa72bef9c04fef8a405372ffb0783b7bd28bce03a6249659697d1c"
   license "MIT"
 
   depends_on "go" => :build
 
   resource "test-heic" do
-    url "https://github.com/comatory/heic-convert/raw/refs/tags/0.1.3/cmd/heic-to-jpeg/testdata/sample.heic"
+    url "https://github.com/comatory/heic-convert/raw/refs/tags/0.1.5/cmd/heic-to-jpeg/testdata/sample.heic"
     sha256 "ee5927b410d586f38fcff146ca1f787ce0a1e04f5ab65c7c62ff1afc31c81e2d"
   end
 
@@ -24,7 +24,7 @@ class ComatoryHeicConvert < Formula
       Run `heic-to-jpeg -h` for usage information.
 
       To install the Automator Quick Action (Finder right-click integration):
-        open "#{pkgshare}/HEIC to JPG.workflow"
+        cp -R "#{pkgshare}/HEIC to JPG.workflow" ~/Library/Services/ && open ~/Library/Services/"HEIC to JPG.workflow"
     EOS
   end
 
