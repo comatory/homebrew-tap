e4530903b98b7a7eed5370c07ba4cb511780e35d
Ondřej Synáček <ondrej@synacek.org>
2026-01-28 21:48:51 +0100

    bump heic-convert version

--- Formula/comatory-heic-convert.rb
+++ Formula/comatory-heic-convert.rb
@@ -1,14 +1,14 @@
 class ComatoryHeicConvert < Formula
   desc "Personalized HEIC to JPEG converter"
   homepage "https://github.com/comatory/heic-convert"
-  url "https://github.com/comatory/heic-convert/archive/refs/tags/0.1.2.tar.gz"
-  sha256 "1128a271c8c076d76deb31178cd26aec2add94d074409408aff0236e8cff29ec"
+  url "https://github.com/comatory/heic-convert/archive/refs/tags/0.1.3.tar.gz"
+  sha256 "0b7b62525322c23127037eb5a22b24be7204aae75be44aca99cc8d6477c2c6e4"
   license "MIT"
 
   depends_on "go" => :build
 
   resource "test-heic" do
-    url "https://github.com/comatory/heic-convert/raw/refs/tags/0.1.2/cmd/heic-to-jpeg/testdata/sample.heic"
+    url "https://github.com/comatory/heic-convert/raw/refs/tags/0.1.3/cmd/heic-to-jpeg/testdata/sample.heic"
     sha256 "ee5927b410d586f38fcff146ca1f787ce0a1e04f5ab65c7c62ff1afc31c81e2d"
   end
 
